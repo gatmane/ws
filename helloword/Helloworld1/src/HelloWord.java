fdccd8d3d4c0ac40564b7b08e4f47eba12289f83
Ghania ATMANE <atm.htc21@gmail.com>
2021-08-23 18:06:03 +0200

    add 2 simple projects

---
+++ helloword/Helloworld1/src/HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world !!!! for mesdd");
+		System.out.println("yhanks !!");
+		List<String> names = new Arralist<String>();
+				
+	}
+
+}
